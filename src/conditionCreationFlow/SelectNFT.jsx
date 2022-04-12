a6fc9cf3b50fdc7cf2bc013fb5a3e36031479b96
zanedr <zanedanielrussell@gmail.com>
2022-04-12 14:10:01 -0600

    change nft copy

--- src/conditionCreationFlow/SelectNFT.jsx
+++ src/conditionCreationFlow/SelectNFT.jsx
@@ -54,29 +54,23 @@ const SelectNFT = ({ setSelectPage, handleUpdateAccessControlConditions }) => {
                          setOption={setChain}
                          turnOffSearch={true}
       />
-      {!tokenId && (
-        <div className={'lsm-w-full'}>
-          <h3 className={'lsm-select-label lsm-text-title-gray lsm-font-segoe lsm-text-base lsm-font-light'}>Select
-            token or
-            enter contract address</h3>
-          <LitTokenSelect option={selectedToken}
-                          label={(!selectedToken || !selectedToken['label']) ? 'Search for a token/NFT' : selectedToken.label}
-                          selectedToken={selectedToken}
-                          setSelectedToken={setSelectedToken}
-          />
-        </div>
-      )}
-      {!selectedToken && (
-        <div className={'lsm-w-full'}>
-          <h3 className={'lsm-select-label lsm-text-title-gray lsm-font-segoe lsm-text-base lsm-font-light'}>Select
-            token or
-            enter contract address</h3>
-          <LitInput value={tokenId}
-                    setValue={setTokenId}
-                    errorMessage={addressIsValid ? null : 'Address is invalid'}
-          />
-        </div>
-      )}
+      <div className={'lsm-w-full'}>
+        <h3 className={'lsm-select-label lsm-text-title-gray lsm-font-segoe lsm-text-base lsm-font-light'}>Select
+          token or
+          enter contract address</h3>
+        <LitTokenSelect option={selectedToken}
+                        label={(!selectedToken || !selectedToken['label']) ? 'Search for a token/NFT' : selectedToken.label}
+                        selectedToken={selectedToken}
+                        setSelectedToken={setSelectedToken}
+        />
+      </div>
+      <div className={'lsm-w-full'}>
+        <h3 className={'lsm-select-label lsm-text-title-gray lsm-font-segoe lsm-text-base lsm-font-light'}>
+          Add Token ID</h3>
+        <LitInput value={tokenId}
+                  setValue={setTokenId}
+        />
+      </div>
       <p
         className={'lsm-text-sm md:lsm-text-base lsm-w-full lsm-mt-8 lsm-cursor-pointer lsm-mb-4 lsm-text-brand-4 lsm-text-left lsm-font-segoe lsm-font-light'}
         onClick={() => setSelectPage('wallet')}>
@@ -84,7 +78,7 @@ const SelectNFT = ({ setSelectPage, handleUpdateAccessControlConditions }) => {
         Blockchain Domain</p>
       <LitFooter backAction={() => setSelectPage('chooseAccess')}
                  nextAction={handleSubmit}
-                 nextDisableConditions={(!chain || !tokenId.length || !selectedToken || !addressIsValid)}/>
+                 nextDisableConditions={(!chain || !tokenId.length || !selectedToken)}/>
     </div>
   );
 };
